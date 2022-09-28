cccaecbff86c0deab50a68d39fb7f42c0597f480
ToxicSodapop <85662710+ToxicSodapop@users.noreply.github.com>
2022-09-28 14:00:37 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,7 @@
+/* Changes background and foreground colours  for all elements on the page */
 function r() { return Math.floor(Math.random() * 255); }
 function fakk(){ this.style.color = ['rgb(',[r(),r(),r()].join(','),')'].join(''),
 this.style.backgroundColor = ['rgb(',[r(),r(),r()].join(','),')'].join(''); }
 [].forEach.call(document.all, function(item) {
-while (true) {
-  setTimeout(function() {fakk.call(item)}, (Math.random()*500)+100)
-}});
+    setTimeout(function() {fakk.call(item)}, (Math.random()*500)+100)
+});
